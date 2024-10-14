99d3151910ffc85dff020a016a5b6121710b209b
aelatris <adhamelatris@yahoo.com>
2024-10-14 00:33:26 -0400

    The game logic is done and everything works perfectly. Only thing left is display size adaptation

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ let rockImg = "https://sihoonathan.github.io/rock-paper-scissors/assets/img/rock
 let paperImg = "play-rock-paper-scissors-with-us-246506.png";
 let scissorsImg = "https://i.pinimg.com/originals/67/eb/fd/67ebfd32e898b4ec3ae4e59ae33832c8.png";
 
+let userScore = document.getElementById("userScore");
+let conputerScore = document.getElementById("computerScore");
+let winLose = document.getElementById("winLose");
+
 
 function displayFirstPart() {
     firstDisplay.style.display = "block";
@@ -44,26 +48,73 @@ function redirectionLoading() {
 let imgArray = [rockImg, paperImg, scissorsImg];
 randomImg =  Math.floor(Math.random() * imgArray.length);
 
+// Function to extract the file name from the full image src URL
+function getFileName(imgSrc) {
+    return imgSrc.substring(imgSrc.lastIndexOf('/') + 1);  // Extract only the file name
+}
+
+let scoreUser = 0;
+let scoreComputer = 0;
+userScore.innerHTML = "Score: " + scoreUser;
+computerScore.innerHTML = "Score: " + scoreComputer;
+
+
 rock.addEventListener("click", function () {
     choice = "Rock";
     redirectionLoading();
-    userImg.src = rockImg
-    randomImg =  Math.floor(Math.random() * imgArray.length);
+    userImg.src = rockImg;
+    randomImg = Math.floor(Math.random() * imgArray.length);
     computerImg.src = imgArray[randomImg];
+
+    if (getFileName(userImg.src) === getFileName(rockImg) && getFileName(computerImg.src) === getFileName(scissorsImg)) {
+        scoreUser = scoreUser + 1;
+        userScore.innerHTML = "Score: " + scoreUser;
+        winLose.innerHTML = "You Win !"
+    }else if(getFileName(userImg.src) === getFileName(rockImg) && getFileName(computerImg.src) === getFileName(paperImg)){
+        scoreComputer = scoreComputer + 1;
+        computerScore.innerHTML = "Score: " + scoreComputer;
+        winLose.innerHTML = "You Lose !"
+    }else{
+        winLose.innerHTML = "EQUAL !";
+    }
 });
 
 paper.addEventListener("click", function () {
     choice = "Paper";
     redirectionLoading();
     userImg.src = paperImg;
-    randomImg =  Math.floor(Math.random() * imgArray.length);
+    randomImg = Math.floor(Math.random() * imgArray.length);
     computerImg.src = imgArray[randomImg];
-});
+
+    if (getFileName(userImg.src) === getFileName(paperImg) && getFileName(computerImg.src) === getFileName(rockImg)) {
+        scoreUser = scoreUser + 1;
+        userScore.innerHTML = "Score: " + scoreUser;
+        winLose.innerHTML = "You Win !"
+    }else if(getFileName(userImg.src) === getFileName(paperImg) && getFileName(computerImg.src) === getFileName(scissorsImg)){
+        scoreComputer = scoreComputer + 1;
+        computerScore.innerHTML = "Score: " + scoreComputer;
+        winLose.innerHTML = "You Lose !"
+    }else{
+        winLose.innerHTML = "EQUAL !";
+    }});
 
 scissors.addEventListener("click", function () {
     choice = "Scissors";
     redirectionLoading();
     userImg.src = scissorsImg;
-    randomImg =  Math.floor(Math.random() * imgArray.length);
+    randomImg = Math.floor(Math.random() * imgArray.length);
     computerImg.src = imgArray[randomImg];
-});
+
+    if (getFileName(userImg.src) === getFileName(scissorsImg) && getFileName(computerImg.src) === getFileName(paperImg)) {
+        scoreUser = scoreUser + 1;
+        userScore.innerHTML = "Score: " + scoreUser;
+        winLose.innerHTML = "You Win !"
+    }else if(getFileName(userImg.src) === getFileName(scissorsImg) && getFileName(computerImg.src) === getFileName(rockImg)){
+        scoreComputer = scoreComputer + 1;
+        computerScore.innerHTML = "Score: " + scoreComputer;
+        winLose.innerHTML = "You Lose !"
+    }else{
+        winLose.innerHTML = "EQUAL !";
+    }});
+
+
