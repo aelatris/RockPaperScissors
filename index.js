c880d3eacad3cf53be8f9c6287144eab5a47d5e0
aelatris <adhamelatris@yahoo.com>
2024-10-13 23:57:35 -0400

    The computer random image logic is now done. Everything works well. Only the game logic is left

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ let secondDisplay = document.getElementById("second");
 let thirdDisplay = document.getElementById("third");
 
 let userImg = document.getElementById("userImg");
+let computerImg = document.getElementById("computerImg");
 let choice = "";
+
+let rockImg = "https://sihoonathan.github.io/rock-paper-scissors/assets/img/rock2.png";
+let paperImg = "play-rock-paper-scissors-with-us-246506.png";
+let scissorsImg = "https://i.pinimg.com/originals/67/eb/fd/67ebfd32e898b4ec3ae4e59ae33832c8.png";
 
 
 function displayFirstPart() {
@@ -36,17 +41,29 @@ function redirectionLoading() {
     }, 2000);
 }
 
+let imgArray = [rockImg, paperImg, scissorsImg];
+randomImg =  Math.floor(Math.random() * imgArray.length);
+
 rock.addEventListener("click", function () {
     choice = "Rock";
     redirectionLoading();
+    userImg.src = rockImg
+    randomImg =  Math.floor(Math.random() * imgArray.length);
+    computerImg.src = imgArray[randomImg];
 });
 
 paper.addEventListener("click", function () {
     choice = "Paper";
     redirectionLoading();
+    userImg.src = paperImg;
+    randomImg =  Math.floor(Math.random() * imgArray.length);
+    computerImg.src = imgArray[randomImg];
 });
 
 scissors.addEventListener("click", function () {
     choice = "Scissors";
     redirectionLoading();
+    userImg.src = scissorsImg;
+    randomImg =  Math.floor(Math.random() * imgArray.length);
+    computerImg.src = imgArray[randomImg];
 });
