a2a71d984522390627707435692dc3053fb7bb05
aelatris <adhamelatris@yahoo.com>
2024-10-13 23:19:46 -0400

    The redirection are done and perfectly functionnal

--- index.js
+++ index.js
@@ -2,24 +2,51 @@ let rock = document.getElementById("rock");
 let paper = document.getElementById("paper");
 let scissors = document.getElementById("scissors");
 
+
+let firstDisplay = document.getElementById("first");
+let secondDisplay = document.getElementById("second");
+let thirdDisplay = document.getElementById("third");
+
+let userImg = document.getElementById("userImg");
 let choice = "";
 
-rock.addEventListener("click", function(){
+
+function displayFirstPart() {
+    firstDisplay.style.display = "block";
+    secondDisplay.style.display = "none";
+    thirdDisplay.style.display = "none";
+}
+
+function displaySecondPart() {
+    firstDisplay.style.display = "none";
+    secondDisplay.style.display = "block";
+    thirdDisplay.style.display = "none";
+}
+
+function displayThirdPart() {
+    firstDisplay.style.display = "none";
+    secondDisplay.style.display = "none";
+    thirdDisplay.style.display = "flex";
+}
+
+function redirectionLoading() {
+    displaySecondPart();
+    setTimeout(function () {
+        displayThirdPart();
+    }, 2000);
+}
+
+rock.addEventListener("click", function () {
     choice = "Rock";
-    alert("You have chosen " + choice);
-
-    window.location.href = "loading.html";
-
+    redirectionLoading();
 });
 
-paper.addEventListener("click", function(){
+paper.addEventListener("click", function () {
     choice = "Paper";
-
-    alert("You have chosen " + choice);
+    redirectionLoading();
 });
 
-scissors.addEventListener("click", function(){
+scissors.addEventListener("click", function () {
     choice = "Scissors";
-
-    alert("You have chosen " + choice);
+    redirectionLoading();
 });
